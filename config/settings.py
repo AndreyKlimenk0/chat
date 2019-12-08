c2a39d6823610209f8ae067695d22b989ca861d6
AndreyKlimenk0 <deadshot271998@gmail.com>
2019-12-08 10:32:32 +0200

    add variable stattic root at settings

--- config/settings.py
+++ config/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
 
 
 REST_FRAMEWORK = {
